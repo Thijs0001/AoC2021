f92720432eb3df4b9d9b978eafc44c53e48ed0fd
Thijs <t.frauenfelder@student.utwente.nl>
2021-12-07 21:22:38 +0100

    finished day2 part2

--- Day2-Dive.py
+++ Day2-Dive.py
@@ -3,7 +3,6 @@ i=""
 while i!="0":
     i=input()
     x=i.split()
-    print(x)
     commands.append(x)
     
 commands.pop()
@@ -18,6 +17,24 @@ while i in range(0,len(commands)):
         depth+=int(commands[i][1])
     elif commands[i][0]=="up":
         depth-=int(commands[i][1])
+    i+=1
+
+print(x)
+print(depth)
+print(x*depth)
+
+i=0
+aim=0
+depth=0
+x=0
+while i in range(0,len(commands)):
+    if commands[i][0]=="forward":
+        x+=int(commands[i][1])
+        depth+=aim*int(commands[i][1])
+    elif commands[i][0]=="down":
+        aim+=int(commands[i][1])
+    elif commands[i][0]=="up":
+        aim-=int(commands[i][1])
     i+=1
 
 print(x)
