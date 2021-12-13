a5ee495d6a8016209b8e8519157896a2d39b0daa
Thijs <t.frauenfelder@student.utwente.nl>
2021-12-13 16:05:49 +0100

    removed weird import

--- Day6/Java/src/Ocean.java
+++ Day6/Java/src/Ocean.java
@@ -1,4 +1,4 @@
-import utils.TextIO;
+import java.util.Scanner;
 
 import java.util.ArrayList;
 
@@ -6,8 +6,9 @@ public class Ocean {
     private static ArrayList<LanternFish> fish = new ArrayList<>();
 
     public static void main(String[] args) {
+        Scanner inputscan = new Scanner(System.in);
         System.out.println("Please input your input");
-        String[] input = "2,5,2,3,5,3,5,5,4,2,1,5,5,5,5,1,2,5,1,1,1,1,1,5,5,1,5,4,3,3,1,2,4,2,4,5,4,5,5,5,4,4,1,3,5,1,2,2,4,2,1,1,2,1,1,4,2,1,2,1,2,1,3,3,3,5,1,1,1,3,4,4,1,3,1,5,5,1,5,3,1,5,2,2,2,2,1,1,1,1,3,3,3,1,4,3,5,3,5,5,1,4,4,2,5,1,5,5,4,5,5,1,5,4,4,1,3,4,1,2,3,2,5,1,3,1,5,5,2,2,2,1,3,3,1,1,1,4,2,5,1,2,4,4,2,5,1,1,3,5,4,2,1,2,5,4,1,5,5,2,4,3,5,2,4,1,4,3,5,5,3,1,5,1,3,5,1,1,1,4,2,4,4,1,1,1,1,1,3,4,5,2,3,4,5,1,4,1,2,3,4,2,1,4,4,2,1,5,3,4,1,1,2,2,1,5,5,2,5,1,4,4,2,1,3,1,5,5,1,4,2,2,1,1,1,5,1,3,4,1,3,3,5,3,5,5,3,1,4,4,1,1,1,3,3,2,3,1,1,1,5,4,2,5,3,5,4,4,5,2,3,2,5,2,1,1,1,2,1,5,3,5,1,4,1,2,1,5,3,5,2,1,3,1,2,4,5,3,4,3".split(",");
+        String[] input = inputscan.nextLine().split(",");
         /*StringBuilder print = new StringBuilder();
         for (String s : input) {
             print.append(s + " ");
@@ -17,7 +18,7 @@ public class Ocean {
             fish.add(new LanternFish(Integer.parseInt(i)));
         }
         System.out.println("How many days would you like to model?");
-        int days = TextIO.getInt();
+        int days = Integer.parseInt(inputscan.nextLine());
         for (int i = days; i > 0; i--) {
             System.out.println(i);
             for (int n = 0; n < fish.toArray().length; n++) {
