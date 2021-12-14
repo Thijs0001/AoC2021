579b0e057d47ba5a36d274306934c73e3c04ec97
Thijs <t.frauenfelder@student.utwente.nl>
2021-12-14 03:09:20 +0100

    lanternfish solutiong with improved speed and memory usage

---
+++ Day6/Day6-LanternFish2.py
@@ -0,0 +1,23 @@
+inputs=input("Please input your input:\n").split(",")
+
+fishes=[0,0,0,0,0,0,0,0,0]
+
+i=0
+while i in range(0,len(inputs)):
+	fishes[int(inputs[i])]+=1
+	i+=1
+
+days=int(input("\nHow many days do you want to simulate?\n"))
+
+i=0
+x=0
+y=0
+z=0
+while i in range(0,days):
+	z=fishes[7]
+	fishes[7]=fishes[8]
+	fishes[8]=fishes[i%7]
+	fishes[i%7]+=z
+	i+=1
+
+print(sum(fishes))
